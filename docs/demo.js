e08ddb63d5601dee85bb4f422d5db5c7939ae18f
Ubuntu <ubuntu@vps-d86a3e06.vps.ovh.net>
2022-03-12 17:33:15 +0000

    Menu dev prototype

--- docs/demo.js
+++ docs/demo.js
@@ -1,24 +1,39 @@
-import { createElement, Component } from "https://cdn.skypack.dev/react";
+import { createElement, Component, useState,useRef } from "https://cdn.skypack.dev/react";
 import { render } from "https://cdn.skypack.dev/react-dom";
 import htm from "https://cdn.skypack.dev/htm";
 import styled from "https://cdn.skypack.dev/styled-components";
+import {ShareApple} from "https://cdn.skypack.dev/@styled-icons/evil"
+import color from 'https://cdn.skypack.dev/color';
+import useToggle from "https://cdn.skypack.dev/@react-hook/toggle"
+import useSize from "https://cdn.skypack.dev/@react-hook/size"
 
 const html = htm.bind(createElement);
 const ControlBox = styled.div`
   background-color: #eae7e3;
-  width: 35vw;
+  width: 65vw;
   height: 35vh;
   margin: 0 auto;
   margin-top: 25vh;
   padding: 5px;
 `;
 const StyledButton = styled.button`
+  background: none;
+	color: inherit;
+	border: none;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+
   border: 1px solid #a9a9a9; // not color ?
   border-radius: 1em;
   transition: all 0.1s;
+  padding: 3px;
+  font-size: 12px;
+  min-width: 25px;
+  min-height: 25px; 
+
   &:active {
-    // transform: rotateY(15deg) rotateX(15deg);
-    // transform: rotate3d(0, 15, 0, 1deg);
     background: linear-gradient(
         0deg,
         rgb(191 191 191 / 44%) 0%,
@@ -27,9 +42,8 @@ const StyledButton = styled.button`
     backdrop-filter: hue-rotate(174deg) invert(70%);
 
     &:before {
-      /* content: ""; */
       position: absolute;
-      /* background-color: rgb(255 255 255 / 0%); */
+
       background: linear-gradient(
         0deg,
         rgb(191 191 191 / 44%) 0%,
@@ -39,16 +53,71 @@ const StyledButton = styled.button`
       width: 65px;
       border-radius: 1em;
       backdrop-filter: hue-rotate(174deg) invert(70%);
-      // top: 170px;
-      // left: 625px;
     }
   }
 `;
 
+
+const DropDownMenu = ({children}) => {
+  return children
+}
+
+
+const StyledPanel = styled.div`
+  position:absolute;
+  /* display:none; */
+`
+
+const MenuPanel = ({children}) => {
+  return html`
+  <${StyledPanel}>
+    ${children}
+  </${StyledPanel}>
+  `
+}
+
+const FloatingTo = ({children})=> {
+  const ref = useRef(null)
+  const [x,y] = useSize(ref)
+  return html ``
+}
+
+const MenuItem = ({name,children}) => {
+  const [isOpen, onToggle] = useToggle(false);
+
+  return html`<span 
+                style=${{
+                  fontSize:'12px',
+                  fontFamily:'-apple-system'
+              }}
+                onClick=${onToggle}
+              >
+                  ${name}
+                  ${isOpen ? html`
+                    <${MenuPanel}>
+                      ${children}
+                    </${MenuPanel}>`
+            :null}
+              </span>`
+}
 const app = html`
       <${ControlBox}>
         <p>Hello World!___</p>
         <${StyledButton}>Click me!</${StyledButton}>
+        <${StyledButton}> <${ShareApple}/> </${StyledButton}>
+        <br/>
+        <${DropDownMenu}>
+          <${MenuItem}
+            name=${html`<span> File</span>`}
+          > 
+            <${MenuItem} name="Open" />
+           </${MenuItem} name="New">
+          <${MenuItem} name='Edit'> 
+            <${MenuItem} name="Open" />
+          </${MenuItem}>
+        </${DropDownMenu}>
+        <br>
+        "test un trc"^
       </${ControlBox}>
       `;
 render(app, document.body);
